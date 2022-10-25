561477abba3e7190e09b0fb2ad45e647662456a3
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-10-25 22:17:36 +0300

    feat: building cart page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { CartComponent } from "./components/pages/cart/cart.component";
 import { HomeComponent } from "./components/pages/home/home.component";
 
 const routes: Routes = [
   {
     path: "home",
     component: HomeComponent,
+  },
+  {
+    path: "cart",
+    component: CartComponent,
   },
   {
     path: "",
---
+++ src/app/components/pages/cart/cart.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Cart, CartItem } from "src/app/models/cart.model";
+
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+})
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product: "https://via.placeholder.com/150",
+        name: "snickers",
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+      {
+        product: "https://via.placeholder.com/150",
+        name: "snickers",
+        price: 150,
+        quantity: 1,
+        id: 2,
+      },
+      {
+        product: "https://via.placeholder.com/150",
+        name: "snickers",
+        price: 150,
+        quantity: 6,
+        id: 3,
+      },
+      {
+        product: "https://via.placeholder.com/150",
+        name: "snickers",
+        price: 150,
+        quantity: 1,
+        id: 4,
+      },
+    ],
+  };
+  dataSource: CartItem[] = [];
+  displayedColumns: string[] = [
+    "product",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: CartItem[]): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((totalPrice, totalItemPrice) => totalItemPrice + totalPrice, 0);
+  }
+}
